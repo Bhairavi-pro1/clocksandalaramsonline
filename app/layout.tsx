9ba655b70024ee865ff255428613e3af56df99ea
sarvesh8939 <ssvvb2004@gmail.com>
2026-03-30 19:24:52 +0530

    up1

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           strategy="lazyOnload"
           crossOrigin="anonymous"
         />
+
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-43759W7BVR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-43759W7BVR');
+          `}
+        </Script>
       </body>
     </html>
   )
